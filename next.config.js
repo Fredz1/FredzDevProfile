a4d1b6b8a35b4a09c3a8c689ba51db98126da480
Fred <fredwil25@gmail.com>
2022-08-09 16:24:20 -0400

    added gtm code

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 const withPWA = require('next-pwa')
-//const siteMap = require('./scripts/generate-sitemap.js')
-
 
 const generateCsp = async() => {
-
-
   return `default-src 'self' www.googletagmanager.com ; style-src 'self' 'unsafe-inline'; script-src 'self' www.googletagmanager.com 'unsafe-eval'; font-src 'self' data:; img-src 'self' www.googletagmanager.com data:; script-src-elem 'self' www.googletagmanager.com 'unsafe-inline'; script-src-attr www.googletagmanager.com `
 }
-
-
 
 module.exports = withPWA(
   {
@@ -20,10 +14,6 @@ module.exports = withPWA(
       sw: 'service-worker.js'
     },
     poweredByHeader: false,
-    devIndicators: {
-      buildActivityPosition: 'bottom-right',
-      autoPrerender: false
-    },
     reactStrictMode: true,
     optimizeFonts: true,
     compress: true,
