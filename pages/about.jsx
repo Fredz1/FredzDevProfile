b13fa7c5d64109852ea7c1af7389ef4e30d9246e
Fred <fredwil25@gmail.com>
2022-08-23 23:22:11 +0200

    styling with css in js

--- pages/about.jsx
+++ pages/about.jsx
@@ -10,10 +10,51 @@ import Image from 'next/image'
 
 const about = () => {
   return (
-    <div className={style.underContructionContainer} >
-      <Image src={underContructionImage} height={400} objectFit='contain' about='underConstruction notification' alt='under Contruction notification'/>
+    <div className='container'>
+      <h2>
+        Hi, I am Fred.  Nice to neet you.
+      </h2>
+      <div>
+        <h3>
+          Designer
+        </h3>
+        <p>
+          I value simple, clean designs and thoughtful interactions.
+        </p>
+
+      </div>
+
+      <div>
+        <h3>
+          Frontend Developer
+        </h3>
+        <p>
+          I like coding things from scratch and enjoy brining ideas to life in the browser.
+        </p>
+      </div>
+
+      <div>
+        <h3>
+          Backend Developer
+        </h3>
+        <p>
+          I enjoy the thought that goes into creating a good, clean and simple backend.
+        </p>
+      </div>
+      <style jsx>{
+      `
+        .container{
+          display: grid;
+          grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+        }
+      `
+    }
+    </style>
     </div>
+    
   )
 }
+
+
 
 export default about
