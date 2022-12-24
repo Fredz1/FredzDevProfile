78b441e8ed8158f64c5b3ccda6a9c59cd0d211b1
Fred <fredwil25@gmail.com>
2022-12-24 20:52:17 +0200

    styles about me section

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,57 +1,58 @@
-// Style
-import style from '../styles/underContruction.module.css'
-
-
 // Assets
-import underContructionImage from '../public/underConstructionImage.png'
+import Grid from '@mui/material/Grid'
+import Container from '@mui/material/Grid'
+import { Typography } from '@mui/material'
 
-// Modules
-import Image from 'next/image'
 
 const about = () => {
   return (
-    <div className='container'>
-      <h2>
-        Hi, I am Fred.  Nice to neet you.
-      </h2>
-      <div>
-        <h3>
-          Designer
-        </h3>
-        <p>
-          I value simple, clean designs and thoughtful interactions.
-        </p>
+    <Container 
+      container
+      direction='column' 
+      spacing={10} 
+      justifyContent="center"
+      alignItems="center"
+      marginTop={5}
+      marginBottom={5}
+    >
 
-      </div>
+    <Grid item>
 
-      <div>
-        <h3>
-          Frontend Developer
-        </h3>
-        <p>
-          I like coding things from scratch and enjoy brining ideas to life in the browser.
-        </p>
-      </div>
+      <Typography variant='h2'>
+        Hi, I am Fred.  Nice to meet you.
+      </Typography>
 
-      <div>
-        <h3>
-          Backend Developer
-        </h3>
-        <p>
-          I enjoy the thought that goes into creating a good, clean and simple backend.
-        </p>
-      </div>
-      <style jsx>{
-      `
-        .container{
-          display: grid;
-          grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-        }
-      `
-    }
-    </style>
-    </div>
+    </Grid>
+      
+    <Grid item rowSpacing={6}>
+
+      <Typography variant='h3' marginY={3}>
+        Designer
+      </Typography>
+      <p>
+        I value simple, clean designs and thoughtful interactions.
+      </p>
+      <Typography variant='h3' marginY={3}>
+        Frontend Developer
+      </Typography>
+      <p>
+        I like coding things from scratch and enjoy brining ideas to life in the browser.
+      </p>
+      <Typography variant='h3' marginY={3}>
+        Backend Developer
+      </Typography>
+      <p>
+        I enjoy the thought that goes into creating a good, clean and simple backend.
+      </p>
+
+      
+
+    </Grid>
+      
     
+    </Container>
+  
+  
   )
 }
 
